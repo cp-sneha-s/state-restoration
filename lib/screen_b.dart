2cb6160c6ee6c7b48e58c150dffdad0d0e4ab208
sneha.s <sneha.s@canopas.com>
2023-03-27 11:53:17 +0530

    Set up initial app

--- lib/screen_b.dart
+++ lib/screen_b.dart
@@ -8,29 +8,45 @@ class ScreenB extends StatefulWidget {
 }
 
 class _ScreenBState extends State<ScreenB> {
+  DateTime selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Screen B'),
+        title: const Text('Screen B'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: const [
-          Center(
-            child: Icon(
-              Icons.gcalendar_month_sharp,
-              size: 50,
-              color: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () async {
+                selectedDate = await openDatePicker(context);
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.calendar_month_sharp,
+                color: Colors.blueGrey,
+              ),
             ),
-          ),
-          Text(
-            'Selected Date',
-            style: TextStyle(fontSize: 25, color: Colors.blueGrey),
-          )
-        ],
+            Text(
+              'Selected Date: ${selectedDate.day}/${selectedDate.month}/${selectedDate.year}',
+              style: const TextStyle(fontSize: 25, color: Colors.blueGrey),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Future<DateTime> openDatePicker(BuildContext context) async {
+    final date = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2022),
+        lastDate: DateTime(2024));
+    return date ?? DateTime.now();
   }
 }
