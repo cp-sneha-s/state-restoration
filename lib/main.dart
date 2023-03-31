a09dde0a4e83167291316b5548757fbcb9789956
sneha.s <sneha.s@canopas.com>
2023-03-31 09:54:52 +0530

    state-restoration with restoration-mixin

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:state_restoration/screen_a.dart';
 import 'package:state_restoration/screen_b.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MaterialApp(
+      restorationScopeId: 'state-restoration', home: HomePage()));
 }
 
 class HomePage extends StatefulWidget {
@@ -13,34 +14,52 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with RestorationMixin {
   final list = [const ScreenA(), const ScreenB()];
-  int _selectedIndex = 0;
+  final RestorableInt _selectedIndex = RestorableInt(0);
+
+  @override
+  void dispose() {
+    _selectedIndex.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'State Restoration',
-      restorationScopeId: 'state-restoration',
-      home: Scaffold(
-        body: list[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: onTap,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Screen A'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.account_balance_wallet), label: 'Screen B')
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('State restoration'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: list[_selectedIndex.value],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.grey[500],
+        currentIndex: _selectedIndex.value,
+        onTap: onTap,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Screen A'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet), label: 'Screen B')
+        ],
       ),
     );
   }
 
   void onTap(int index) {
-    if (_selectedIndex != index) {
+    if (_selectedIndex.value != index) {
       setState(() {
-        _selectedIndex = index;
+        _selectedIndex.value = index;
       });
     }
+  }
+
+  @override
+  // TODO: implement restorationId
+  String? get restorationId => 'bottom-state';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedIndex, 'main');
   }
 }
--- lib/screen_a.dart
+++ lib/screen_a.dart
@@ -7,7 +7,7 @@ class ScreenA extends StatefulWidget {
   State<ScreenA> createState() => _ScreenAState();
 }
 
-class _ScreenAState extends State<ScreenA> with RestorationMixin {
+class _ScreenAState extends State<ScreenA> with RestorationMixin<ScreenA> {
   final ScrollController _scrollController = ScrollController();
   final RestorableDouble _scrollOffset = RestorableDouble(0);
   @override
@@ -17,10 +17,12 @@ class _ScreenAState extends State<ScreenA> with RestorationMixin {
   @override
   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
     registerForRestoration(_scrollOffset, 'Scroll-position');
+    print(_scrollOffset.value);
   }
 
   @override
   void initState() {
+    print('init state is called');
     super.initState();
     _scrollController.addListener(() {
       _scrollOffset.value = _scrollController.offset;
@@ -39,28 +41,22 @@ class _ScreenAState extends State<ScreenA> with RestorationMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Screen A'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: ListView.separated(
-            controller: _scrollController,
-            itemBuilder: (context, index) {
-              return ListTile(
-                tileColor: Colors.yellow,
-                title: Center(child: Text('Item $index')),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(
-                height: 15,
-              );
-            },
-            itemCount: 100),
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: ListView.separated(
+          controller: _scrollController,
+          itemBuilder: (context, index) {
+            return ListTile(
+              tileColor: Colors.yellow,
+              title: Center(child: Text('Item $index')),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(
+              height: 15,
+            );
+          },
+          itemCount: 100),
     );
   }
 }
